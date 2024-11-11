b9a045c2835d4705fcb733dab3f23d478cc94cb3
Mirosław Stanek <froger.mcs@gmail.com>
2024-11-11 09:45:48 +0100

    chores

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codebase-dump",
-    version="0.1.0",
+    version="0.1.1",
     description="Generate a single-file dump of your repository, so you can use it as LLM input",
     author="Mirek Stanek, Kamil Stanuch",
     author_email="mirek@practicalengineering.management, kamil@stanuch.eu",
@@ -10,7 +10,7 @@ setup(
     package_dir={"": "src"},
     install_requires=["tiktoken"],  # List core dependencies here
     extras_require={
-        "dev": ["pytest"]  # Development dependencies
+        "dev": ["pytest", "twine"]  # Development dependencies
     },
     python_requires=">=3.7",
 )
--- src/codebase_dump/__init__.py
+++
@@ -1,3 +0,0 @@
-# from core.codebase_analysis import CodebaseAnalysis
-# from core.ignore_patterns_manager import IgnorePatternsManager
-# from core.models import DirectoryAnalysis, FileAnalysis, NodeAnalysis
