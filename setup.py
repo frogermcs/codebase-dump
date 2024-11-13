2e8a966ca5867652186a7915aec51eb1bcae84e4
Mirosław Stanek <froger.mcs@gmail.com>
2024-11-13 22:02:17 +0100

    Bump version to v.0.1.6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="codebase-dump",
-    version="0.1.6",
+    version="0.1.7",
     description="Generate a single-file dump of your repository, so you can use it as LLM input",
     author="Mirek Stanek, Kamil Stanuch",
     author_email="mirek@practicalengineering.management, kamil@stanuch.eu",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["tiktoken"],  # List core dependencies here
+    install_requires=["tiktoken", "gitignore_parser"],  # List core dependencies here
     extras_require={
         "dev": ["pytest", "twine"]  # Development dependencies
     },
