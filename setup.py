c4018d93e53549798c1788d674f9e8ca9c78a19b
Mirosław Stanek <froger.mcs@gmail.com>
2024-12-15 11:41:14 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codebase-dump",
-    version="0.2.1",
+    version="0.2.2",
     description="Generate a single-file dump of your repository, so you can use it as LLM input",
     author="Mirek Stanek, Kamil Stanuch",
     author_email="mirek@practicalengineering.management, kamil@stanuch.eu",
