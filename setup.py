d5c7c707cdb4e4a5beb6724d69bbca381784b299
Mirosław Stanek <froger.mcs@gmail.com>
2025-01-28 17:33:07 +0100

    v0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codebase-dump",
-    version="0.5.0",
+    version="0.5.1",
     description="Parse your repository into single-file prompt, so you can use it as an LLM input.",
     author="Mirek Stanek, Kamil Stanuch",
     author_email="mirek@practicalengineering.management, kamil@stanuch.eu",
